d7d13d206d352a13fe0d3f811be0ab349ca1aa42
GFC-01 <mhlopenkosikhona21@gmail.com>
2025-10-17 18:04:15 +0200

    feat: add Event Serializer

--- events/serializer.py
+++ events/serializer.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from .models import model_for_events_to_be_created_by_West
+from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
-        model = model_for_events_to_be_created_by_West
-        fields = [] # fields to be exposed in our API
+        model = Event
+        fields = ['name', 'location', 'datetime', 'description'] # fields to be exposed in our API
 
