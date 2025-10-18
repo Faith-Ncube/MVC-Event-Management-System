e6a969552d0f048820ea379aa296261e181968da
Nkosikhona Mhlope <mhlopenkosikhona21@gmail.com>
2025-10-18 10:01:41 +0000

    feat(api): Add API views for events and attendees
    - Implemented API views using DRF's generic generic classes for CRUD operations.
    - Added a custom view to list all attendees for a specifc event.

--- events/views.py
+++ events/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Event, Registration
+from .serializers import EventSerializer, RegistrationSerializer
 
-# Create your views here.
+class EventListCreateAPIView(generics.ListCreateAPIView):
+    """
+    API view to list all events or create a new event.
+    - GET: Returns a list of all events.
+    - POST: Create a new event.
+    """
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class EventRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API view to retrieve, update or delete a single event by its ID.
+    - GET: Retrieve details of a specific event.
+    - PUT/PTACH: Update a specific event.
+    - DELETE: Delete a specific  event.
+    """
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class EventAttendeesAPIView(generics.ListAPIView):
+    """
+    This view returns a list of all attendees registered for a specific event.
+    """
+    serializer_calss = RegistrationSerializer
+
+    def get_quesryset(self):
+        # This method overrides the default queryset to filter registrations
+        # based on the event's primary key ('event_pk') passed in the URL.
+        event_id = self.kwargs['event_pk']
+        return Registration.objects.filter(event_id=event_id)
