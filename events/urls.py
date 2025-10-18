f7e0a5be11fe171f7d84c1fe56ef0110720b3879
Nkosikhona Mhlope <mhlopenkosikhona21@gmail.com>
2025-10-18 10:24:39 +0000

    feat(api): Create URL patterns for the API
    - Added a new urls.py to the events app.
    Defined the URL patterns to map the API views to their endpoints.

---
+++ events/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/events/', views.EventListCreateAPIView.as_view(), name='api-event-list-create'),
+    path('api/events/<int:pk>/', views.EventRetrieveUpdateDestroyAPIView.as_view(), name='api-event-retrieve-update-delete'),
+
+    path('api/events/<int:event_id>/attendees', views.EventAttendeesAPIView.as_view(), name='api-event-attendees')
+]
